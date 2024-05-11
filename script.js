d414834cb6e9772dea2a888015158f718ba5a428
Javier <javie.silvav@duocuc.cl>
2024-05-11 16:44:00 -0400

    Validaciones contraseñas y telefono
    que tenga 8 caracteres mínimo y una mayúscula. que tenga 8 dígitos como mínimo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validarFormulario(event) {
     var telefono = $('#phone'); 
     var valorTelefono = telefono.val();
     var numeros = /^[0-9]+$/;
-    if (!numeros.test(valorTelefono)) {
+    if (!numeros.test(valorTelefono) || valorTelefono.length > 8) {
         $('#phone-error').css('display', 'block');
         event.preventDefault();
         return;
@@ -14,12 +14,23 @@ function validarFormulario(event) {
     // Validar contraseña
     var contraseña = $('#password').val();
     var confirmarContraseña = $('#confirm_password').val();
+
+    var contraseñaRegex = /^(?=.*[A-Z]).{8,}$/;
+
     if (contraseña !== confirmarContraseña) {
-        $('#password-error').css('display', 'block');
+        $('#password-error').css('display', 'block').text('Las contraseñas no coinciden.');
         event.preventDefault();
         return;
     } else {
         $('#password-error').css('display', 'none');
+    }
+
+    if (!contraseñaRegex.test(contraseña)) {
+        $('#password-error-two').css('display', 'block').text('La contraseña debe tener al menos 8 caracteres y contener al menos una letra mayúscula.');
+        event.preventDefault();
+        return;
+    } else {
+        $('#password-error-two').css('display', 'none');
     }
 
     $('#exitoModal').modal('show');
