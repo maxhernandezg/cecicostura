c68e006c3bb2fad65214d6499e850e048833b23a
JoaquinFerrada93 <133885139+elmatatan45@users.noreply.github.com>
2024-05-31 18:30:17 -0400

    sdasasdasdasd

--- script.js
+++ script.js
@@ -51,3 +51,14 @@ document.addEventListener('DOMContentLoaded', () => {
       navLinks.classList.toggle('active');
     });
   });
+
+
+
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
+  
